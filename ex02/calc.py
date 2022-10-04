116b158f35d39b9798ca4a69e687ef6dca2671b5
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-04 15:27:10 +0900

    電卓の練習7

--- ex02/calc.py
+++ ex02/calc.py
@@ -15,6 +15,12 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     entry.insert(tk.END, num)
+    
+def button_eq(event):
+    eqn = entry.get()
+    res = eval(eqn)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
 
 numbers = list(range(9, -1, -1))
 operators = ["+"]
@@ -30,7 +36,9 @@ for i,num in enumerate(numbers + operators, 1):
     if i%3 == 0:
         r+=1
         c=0                    
-                    
-
-
+eq = tk.Button(root, text="=",
+                      font = ("Times New Roman", 30),
+                      width=4, height=2)
+eq.bind("<1>", button_eq)
+eq.grid(row = r, column = c)                    
 root.mainloop()
