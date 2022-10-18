6477d59e69ed610eaa34bf6e4ffb7bcd7b722800
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-18 14:11:04 +0900

    練習問題3-2

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("迷えるこうかどん")
-root.geometry("1000x900")
-root.mainloop();
+root.title("迷えるこうかどん") #練習3-1
+cv = tk.Canvas(root, width=1500, height=900, bg="black")
+cv.pack()
+
+root.mainloop()
 
