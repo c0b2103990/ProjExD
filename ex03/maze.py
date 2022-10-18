b0a031b2f2a785f9f9163e97d603bed791f969f3
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-18 14:30:27 +0900

    練習問題3-6

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,10 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up(event):
+    global key
+    key = ""
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかどん") #練習3-1
@@ -16,6 +20,7 @@ if __name__ == "__main__":
 
     key = ""
 
-    root.bind("<keyPress>", key_down)
+    root.bind("<KeyPress>", key_down)
+    root.bind("KeyRelease", key_up)
     root.mainloop()
 
