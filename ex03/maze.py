c63cbec3aa2b0212066d8447052a9a172c8f9a2b
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-18 14:19:06 +0900

    練習問題3-3

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,9 +1,13 @@
 import tkinter as tk
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかどん") #練習3-1
+    cv = tk.Canvas(root, width=1500, height=900, bg="black")
+    cv.pack()
 
-root = tk.Tk()
-root.title("迷えるこうかどん") #練習3-1
-cv = tk.Canvas(root, width=1500, height=900, bg="black")
-cv.pack()
+    tori = tk.PhotoImage(file="ex03/fig/8.png")
+    cx, cy = 300, 400
+    cv.create_image(cx, cy, image=tori, tag="tori")
 
-root.mainloop()
+    root.mainloop()
 
