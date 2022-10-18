650d6ec9333c16b504c8685a0eee7787a66f4b93
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-18 15:37:26 +0900

    こうかどんの画像変更

--- ex03/maze.py
+++ ex03/maze.py
@@ -20,9 +20,9 @@ def main_proc():
         mx -= 1
     if key == "Right":
         mx += 1
-    if maze_lst[my][mx] == 0: # 床なら
+    if maze_lst[my][mx] == 0: 
         cx, cy = mx*100+50, my*100+50
-    else: # 壁なら
+    else: 
         if key == "Up":
             my += 1
         if key == "Down":
@@ -36,15 +36,14 @@ def main_proc():
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかどん") #練習3-1
+    root.title("迷えるこうかどん")
     cv = tk.Canvas(root, width=1500, height=900, bg="black")
     cv.pack()
 
-    maze_lst = mm.make_maze(15, 9)
-    
+    maze_lst = mm.make_maze(15, 9)    
     mm.show_maze(cv, maze_lst)
 
-    tori = tk.PhotoImage(file="ex03/fig/8.png")
+    tori = tk.PhotoImage(file="ex03/fig/2.png")
     mx, my = 1, 1
     cx, cy = mx * 100 + 50, my * 100 + 50
     cv.create_image(cx, cy, image=tori, tag="tori")
@@ -54,9 +53,6 @@ if __name__ == "__main__":
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     main_proc()
-
-
-
 
     root.mainloop()
 
