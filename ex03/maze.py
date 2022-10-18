1557fe77b1939be56cdafc7afcf23ef7c1e974ac
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-18 14:51:03 +0900

    練習問題3-10

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker as mm
 
 def key_down(event):
     global key
@@ -36,6 +37,11 @@ if __name__ == "__main__":
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     main_proc()
+
+    maze_lst = mm.make_maze(15, 9)
+    
+    mm.show_maze(cv, maze_lst)
+
 
 
     root.mainloop()
