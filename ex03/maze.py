0933f8c6ffcb3ddfea52e10f789606217cd1ea99
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-18 16:25:59 +0900

    startとゴールwo

--- ex03/maze.py
+++ ex03/maze.py
@@ -43,10 +43,14 @@ if __name__ == "__main__":
     maze_lst = mm.make_maze(15, 9)    
     mm.show_maze(cv, maze_lst)
 
+    label = tk.Label(root,  text="Start", font=("ricty Diminished", 20))
+    label.place(x=110, y=130)
+
     tori = tk.PhotoImage(file="ex03/fig/2.png")
     mx, my = 1, 1
     cx, cy = mx * 100 + 50, my * 100 + 50
     cv.create_image(cx, cy, image=tori, tag="tori")
+
 
     key = ""
 
