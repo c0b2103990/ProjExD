e952e4350df1909c93b8b35e873eaaef0194ba9b
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-25 15:53:01 +0900

    加速機能の追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -64,6 +64,8 @@ def main():
         yoko, tate = check_bound(bomb_rct,scrn_rct)
         vx *= yoko
         vy *= tate
+        vx *= 1.001 #加速
+        vy *= 1.001
         bomb_rct.move_ip(vx, vy)
 
         if tori_rct.colliderect(bomb_rct):
