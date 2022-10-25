d12a2cbb19e7388a86a7f650238cf89004bb2878
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-25 14:59:30 +0900

    練習問題4-5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,9 +1,11 @@
 import pygame as pg
 import sys
+from random import randint
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
     bg_sfc  = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
@@ -12,10 +14,17 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
 
+    bomb_sfc = pg.Surface((20, 20))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc,(255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx, bomb_rct.centery = randint(0, scrn_rct.width), randint(0, scrn_rct.height)
+
     clock = pg.time.Clock()
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
         scrn_sfc.blit(tori_sfc, tori_rct)
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
