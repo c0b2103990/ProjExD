6e0fff02a8d0dbe8090de994d373b655dd6b4629
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-25 14:27:32 +0900

    練習問題4-3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,17 +6,20 @@ def main():
     scrn_sfc = pg.display.set_mode((1600, 900))
     bg_sfc  = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
-    # tori_sfc = pg.image.load("fig/8.png")
-    # tori_rct = tori_sfc.get_rect()
-    # tori_rct.center = 700, 400
-    # scrn_sfc.blit(tori_sfc, tori_rct)
+
+    tori_sfc = pg.image.load("fig/8.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+
     clock = pg.time.Clock()
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
-        pg.display.update()
+        scrn_sfc.blit(tori_sfc, tori_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        pg.display.update()
         clock.tick(1000)
 
 
