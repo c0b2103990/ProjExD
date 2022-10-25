dfc10ebd9e3f96bb318f6cfd8d45d79583ed6256
C0B21039 <c0b2103990@edu.teu.ac.jp>
2022-10-25 17:09:18 +0900

    ゲームオーバーの追加

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -19,6 +19,9 @@ def main():
     bg_sfc  = pg.image.load("fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
 
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render(str("Game Over"), True, "RED")
+
     tori_sfc = pg.image.load("fig/8.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
@@ -72,12 +75,15 @@ def main():
         vy *= 1.001
         bomb_rct.move_ip(vx, vy)
         bomb_rct2.move_ip(vx, vy)
+
         if tori_rct.colliderect(bomb_rct):
-            return
+            scrn_sfc.blit(txt,(600, 400))
+            pg.time.wait(1000)
         if tori_rct.colliderect(bomb_rct2):
-            return
+            scrn_sfc.blit(txt,(600, 400))
+            pg.time.wait(1000)
         pg.display.update()
-        clock.tick(1000)
+        clock.tick(10000)
 
 
 if __name__ == "__main__":
